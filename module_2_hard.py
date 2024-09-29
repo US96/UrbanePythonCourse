9bb78b7d9766515388ba958d22a08a88e3994e3b
Aleksey Usach <70377090+US96@users.noreply.github.com>
2024-09-29 10:29:10 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,1 +1,14 @@
+def generate_password(n):
+    result = ""
+    # Цикл для перебора всех возможных пар чисел
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            # Если сумма пары делится на n без остатка
+            if (i + j) % n == 0:
+                result += f"{i}{j}"  # Добавляем пару к результату
+    return result
 
+
+# Пример вызова функции с числом 9
+n = int(input("Введите число от 3 до 20: "))
+print(generate_password(n))
