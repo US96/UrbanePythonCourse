22836b4fcdce53d2a8e778695d73796f9307787a
Aleksey Usach <70377090+US96@users.noreply.github.com>
2024-10-03 06:05:05 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,11 +1,16 @@
-def generate_password(n):
-    result = ""
-    # Цикл для перебора всех возможных пар чисел
+def find_password(n):
+    password = ''  # Пустая строка для хранения результата
+
+    # Перебираем все возможные пары чисел от 1 до n
     for i in range(1, n):
         for j in range(i + 1, n + 1):
-            # Если сумма пары делится на n без остатка
-            if (i + j) % n == 0:
-                result += f"{i}{j}"  # Добавляем пару к результату
-    return result
+            if n % (i + j) == 0:  # Проверяем кратность числа n сумме пары
+                password += str(i) + str(
+                    j)  # Добавляем пару в строку результата
+
+    return password
+
+
+# Пример использования:
 n = int(input("Введите число от 3 до 20: "))
-print(generate_password(n))
+print(f"Нужный пароль: {find_password(n)}")
