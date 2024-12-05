4798893a36c6ac17e7c6190d08921427499311cf
Aleksey Usach <70377090+US96@users.noreply.github.com>
2024-12-05 05:34:16 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -1,20 +1,27 @@
+# Создание класса House
 class House:
+    # Метод __init__ для инициализации объекта
     def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = number_of_floors
+        self.name = name  # Название дома
+        self.number_of_floors = number_of_floors  # Количество этажей
 
+    # Метод для перемещения на указанный этаж
     def go_to(self, new_floor):
+        # Проверка на допустимость этажа
         if 1 <= new_floor <= self.number_of_floors:
+            # Вывод номеров этажей от 1 до new_floor
             for floor in range(1, new_floor + 1):
                 print(floor)
         else:
             print("Такого этажа не существует")
 
 
-# Примеры использования
-if __name__ == "__main__":
-    h1 = House('ЖК Горский', 18)
-    h2 = House('Домик в деревне', 2)
+# Создание объектов класса House
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
 
-    h1.go_to(5)
-    h2.go_to(10)
+# Вызов метода go_to для объекта h1
+h1.go_to(5)
+print("---")
+# Вызов метода go_to для объекта h2
+h2.go_to(10)
